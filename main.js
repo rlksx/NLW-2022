21e2b60ea6d9c21d5b1a80d7a3cdd4e158a5d200
Ryan Gabriel <ryanb@pop-os.localdomain>
2022-05-10 17:32:30 -0300

    until-03

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 // onScroll = variável criada
 // scrollY = eixo Y do rolamento
-// scroll = classe crianda no css e sendo adicionada ou removida pelo js.
+// scroll = classe criada no css e sendo adicionada ou removida pelo js.
 
 // openMenu = variável criada
 // closeMenu = variável criada
 // menu-expanded = classe do menu expandido criada no css.
-
-// ALT+SHIFT+DIRECTION = descubra!
 function onScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
@@ -14,6 +12,7 @@ function onScroll() {
     navigation.classList.remove('scroll')
   }
 }
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -22,4 +21,17 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
 
-
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 900,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about, 
+  #about header, 
+  #about .content`)
